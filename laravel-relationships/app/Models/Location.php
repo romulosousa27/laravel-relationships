d2b4c04ff83004243580b0eae7627c1610a5c315
Romulo Sousa <romulosousa27@hotmail.com>
2017-11-16 21:17:07 -0300

    fillable localizacao do pais

--- laravel-relationships/app/Models/Location.php
+++ laravel-relationships/app/Models/Location.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model{
+
+    protected $fillable = ['latitude', 'longitude'];
     
     public function country(){
     	return $this->belongsTo(Country::class);
