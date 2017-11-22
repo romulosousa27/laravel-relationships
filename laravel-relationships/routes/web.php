8f676cb0f12566d670df47bcab7840649fdf8e68
Romulo Sousa <romulosousa27@hotmail.com>
2017-11-21 21:37:41 -0300

    adicionado rota para relacionamento one to many

--- laravel-relationships/routes/web.php
+++ laravel-relationships/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function(){
+    return "Laravel Relationship";
+});
+
 /*
  * Routes -  One To One
  */
@@ -7,6 +11,14 @@ $this->get('one-to-one', 'OneToOneController@OneToOne');
 $this->get('one-to-one-inverse', 'OneToOneController@OneToOneInverse');
 $this->get('one-to-one-insert', 'OneToOneController@OneToOneInsert');
 
+/*
+    Relacionamento 1 para muitos - One To Many
+*/
+$this->get('one-to-many', 'OneToManyController@OneToMany');
+
+
+/* Rota padrão do Laravel
 Route::get('/', function () {
     return view('welcome');
 });
+*/
