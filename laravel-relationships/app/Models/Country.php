6142378a62d05e5aed64e9e42e884630c44515e7
Romulo Sousa <romulosousa27@hotmail.com>
2017-12-04 22:03:00 -0300

    metodo de estado

--- laravel-relationships/app/Models/Country.php
+++ laravel-relationships/app/Models/Country.php
@@ -11,10 +11,12 @@ class Country extends Model{
     
     //Procura a localização do pais atraves da longitude e latitude
     public function location(){
+//      retornando apenas 1 dado vinculado a apenas 1 outro dado.
         return $this->hasOne(Location::class);
     }
     
     public function states() {
+//       retorna varios dados vinculado a apenas 1. 
         return $this->hasMany(State::class);
     }
 }
