c60f3863176713550ea4ec1527813eafb3dd69ef
Romulo Sousa <romulosousa27@hotmail.com>
2018-01-08 23:16:53 -0300

    fillable model states para o OneToManyInsert

--- laravel-relationships/app/Models/State.php
+++ laravel-relationships/app/Models/State.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class State extends Model{
+
+    protected $fillable = ['name', 'initials'];
     
     public function country() {
         return $this->belongsTo(Country::class);
