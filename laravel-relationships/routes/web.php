7310a8f103b7e29d034a104ee24409882efe2f24
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-05 20:12:34 -0300

    refatorando

--- laravel-relationships/routes/web.php
+++ laravel-relationships/routes/web.php
@@ -3,25 +3,3 @@
 Route::get('/', function(){
     return "Laravel Relationship";
 });
-
-/*
- * Routes -  One To One
- */
-$this->get('one-to-one', 'OneToOneController@OneToOne');
-$this->get('one-to-one-inverse', 'OneToOneController@OneToOneInverse');
-$this->get('one-to-one-insert', 'OneToOneController@OneToOneInsert');
-
-/*
-    Relacionamento 1 para muitos - One To Many
-*/
-$this->get('one-to-many', 'OneToManyController@OneToMany');
-$this->get('many-to-one', 'OneToManyController@ManyToOne');
-$this->get('one-to-many-two', 'OneToManyController@OneToManyTwo');
-$this->get('one-to-many-insert', 'OneToManyController@OneToManyInsert');
-
-
-/* Rota padrão do Laravel
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
