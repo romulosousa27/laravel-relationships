1f901128bcfc6495cfd5574c1043c9aebeaade2f
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-06 14:43:35 -0300

    localizacao model

--- laravel-relationships/app/Models/Localizacao.php
+++ laravel-relationships/app/Models/Localizacao.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Localizacao extends Model
 {
+    protected $fillable = ['latitude', 'longitude'];
+
     public function Pais(){
         return $this->belongsTo(Pais::class);
     }
