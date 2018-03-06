609a16203a02ba41d5a589a68f1197433fa03617
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-06 20:26:57 -0300

    relacionamento de estado para o pais

--- laravel-relationships/app/Models/Pais.php
+++ laravel-relationships/app/Models/Pais.php
@@ -11,4 +11,7 @@ class Pais extends Model
     public function local(){
         return $this->hasOne(Localizacao::class);
     }
+    public function estados(){
+        return $this->hasMany(Estado::class);
+    }
 }
