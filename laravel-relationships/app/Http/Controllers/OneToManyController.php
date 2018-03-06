44698f5c627ac4f704d2a0614db0274ccab92a54
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-06 20:26:23 -0300

    trazendo estados do pais

--- laravel-relationships/app/Http/Controllers/OneToManyController.php
+++ laravel-relationships/app/Http/Controllers/OneToManyController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pais;
+use App\Models\Estado;
 
 class OneToManyController extends Controller
 {
     public function OneToMany(){
+        //$pais = Pais::where('name', 'Brasil')->get()->first();
+        $pesquisa = 'a';
+        $paises = Pais::where('name', 'LIKE', "%{$pesquisa}%")->get();
+
+        foreach ($paises as $p){
+            //exibindo o nome do pais.
+            echo "<b> {$p->name} </b>";
+
+            //exibindo todos os estado do pais.
+            $estados = $p->estados()->get();
+            foreach ($estados as $e){
+                echo "<br>{$e->name} ({$e->sigla})";
+            }
+            echo "<hr>";
+        }
 
     }
 }
