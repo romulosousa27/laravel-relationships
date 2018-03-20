f3e680da614471f66125557206eec537742c7fb2
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-20 16:33:30 -0300

    metodo inverso de many to many - empresa para cidades

--- laravel-relationships/app/Http/Controllers/ManyToManyController.php
+++ laravel-relationships/app/Http/Controllers/ManyToManyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cidade;
+use App\Models\Empresa;
 
 class ManyToManyController extends Controller
 {
@@ -14,6 +15,16 @@ class ManyToManyController extends Controller
         $empresas = $cidade->empresas;
         foreach ($empresas as $e){
             echo "{$e->name}";
+        }
+    }
+
+    public function ManyToManyInverso(){
+        $empresa = Empresa::where('name', 'Caju Flex')->get()->first();
+        echo "<b>{$empresa->name}</b>: ";
+
+        $cidades = $empresa->cidades;
+        foreach ($cidades as $c){
+            echo "{$c->name}";
         }
     }
 }
