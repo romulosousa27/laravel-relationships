736cb69929774a20a77070042355749e5d9fa828
Romulo Sousa <romulosousa27@hotmail.com>
2018-03-20 16:13:38 -0300

    controller parao relacionamento de empresas e cidades

--- laravel-relationships/app/Http/Controllers/ManyToManyController.php
+++ laravel-relationships/app/Http/Controllers/ManyToManyController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cidade;
 
 class ManyToManyController extends Controller
 {
     public function ManyToMany(){
-        
+        $cidade = Cidade::where('name', 'Fortaleza')->get()->first();
+        echo "<b>{$cidade->name}</b>: ";
+
+        $empresas = $cidade->empresas;
+        foreach ($empresas as $e){
+            echo "{$e->name}";
+        }
     }
 }
